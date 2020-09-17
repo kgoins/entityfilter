2d2ed9662d20904009fc69a9ac2fffbae3827797
kgoins <kgoins@rule10.net>
2020-09-17 12:18:17 -0400

    added test for complex matching results and modded GetMatches to return the underlying value of reflect.Value instead of reflect.Value

--- entityfilter/matcher/entitymatcher/entity_matcher_test.go
+++ entityfilter/matcher/entitymatcher/entity_matcher_test.go
@@ -36,3 +36,28 @@ func TestEntityMatcher_MatchSingleFilterMultTypes(t *testing.T) {
 		t.Fatal("Wrong number of matches returned")
 	}
 }
+
+func TestEntityMatcher_CompositeFilterMultResults(t *testing.T) {
+	testFilterStr := "myint:!=0,mystr:~hello"
+
+	testStructs := []TestStruct{
+		TestStruct{myint: 0, mystr: "hello"},
+		TestStruct{myint: 1, mystr: "hello world"},
+		TestStruct{myint: 2, mystr: "hello world!"},
+	}
+
+	testFilter, err := filter.ParseFilterStr(testFilterStr)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	matcher := entitymatcher.NewEntityMatcher(testStructs)
+	results, err := matcher.GetMatches(testFilter...)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if len(results) != 2 {
+		t.Fatal("Wrong number of matches returned")
+	}
+}
